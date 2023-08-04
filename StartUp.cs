fce25398a8e2839eaaaea4ca8cec087f76ced6dc
Andyke87 <114744748+Andyke87@users.noreply.github.com>
2023-08-04 12:54:47 +0200

    Add files via upload

---
+++ StartUp.cs
@@ -0,0 +1,27 @@
+﻿using TorensVanHanoiDomein;
+using Cui;
+
+namespace StartUp
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string antwoord;
+            do
+            {
+                DomeinController dc = new DomeinController();
+                TorensVanHanoiApp app = new TorensVanHanoiApp(dc);
+                app.Start();
+
+                Console.WriteLine("\nWilt u nog eens spelen?");
+                Console.Write("Geef ja in of eender welke toets om te stoppen: ");
+                antwoord = Console.ReadLine();
+                Console.WriteLine();
+            } while (antwoord.ToLower().Trim() == "ja");
+
+            Console.WriteLine("Bedankt voor te spelen, tot de volgende keer!");
+
+        }
+    }
+}
